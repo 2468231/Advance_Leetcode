c7ed72a19922e192aa73ce029e0067570f497646
Hemantha <hhemantha696@gmail.com>
2026-07-06 09:37:41 +0530

    Time: 0 ms (100%), Space: 15.1 MB (82.12%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto &a, auto &b){
+            if(a[0] == b[0]){
+                return a[1] > b[1];
+            }
+            return a[0] < b[0];
+        });
+
+        int n = intervals.size();
+        int maxel = intervals[0][1];
+        int count = n;
+        for(int i=1; i<n; i++){
+            if(intervals[i][1] <= maxel){
+                count--;
+            }
+            else{
+                maxel = max(maxel, intervals[i][1]);
+            }
+        }
+    return count;
+    }
+};
