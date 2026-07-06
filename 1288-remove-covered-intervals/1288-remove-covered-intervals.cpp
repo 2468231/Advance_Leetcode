8ece101594bf02c12e06fa9ce07da30b8cf258ee
Hemantha <hhemantha696@gmail.com>
2026-07-06 09:38:29 +0530

    Time: 0 ms (100%), Space: 15.2 MB (60.13%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -3,7 +3,7 @@ public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end(), [](auto &a, auto &b){
             if(a[0] == b[0]){
-                return a[1] > b[1];
+                return a[1] > b[1];  //For Edge case, You think that edge case
             }
             return a[0] < b[0];
         });
