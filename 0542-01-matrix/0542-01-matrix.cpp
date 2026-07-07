11224c9f2e166395d586810b9a0eaa1f8cf0557b
Hemantha <hhemantha696@gmail.com>
2026-07-07 11:41:43 +0530

    Time: 23 ms (40.36%), Space: 38.8 MB (30.15%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+
+        int n = mat.size();
+        int m = mat[0].size();
+
+        // Visited array
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        // Distance array
+        vector<vector<int>> dist(n, vector<int>(m, 0));
+
+        // Queue -> {{row, col}, distance}
+        queue<pair<pair<int, int>, int>> q;
+
+        // Push all 0's into the queue
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (mat[i][j] == 0) {
+                    q.push({{i, j}, 0});
+                    vis[i][j] = 1;
+                }
+            }
+        }
+
+        // Directions
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
+
+        // BFS
+        while (!q.empty()) {
+
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+
+            q.pop();
+
+            dist[row][col] = steps;
+
+            for (int i = 0; i < 4; i++) {
+
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+
+                if (nrow >= 0 && nrow < n &&
+                    ncol >= 0 && ncol < m &&
+                    vis[nrow][ncol] == 0) {
+
+                    vis[nrow][ncol] = 1;
+                    q.push({{nrow, ncol}, steps + 1});
+                }
+            }
+        }
+
+        return dist;
+    }
+};
