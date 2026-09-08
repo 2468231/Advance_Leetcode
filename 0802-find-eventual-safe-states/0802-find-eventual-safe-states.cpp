c1ba31d323d51f0cb820f469d8bfb0de9bf1f4bd
Hemantha <hhemantha696@gmail.com>
2026-09-08 23:41:57 +0530

    Time: 54 ms (28.83%), Space: 66.4 MB (20.88%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+
+        int V = graph.size();
+
+        vector<vector<int>> adjRev(V);
+        vector<int> indegree(V, 0);
+
+        // Reverse the graph
+        for (int i = 0; i < V; i++) {
+            for (auto it : graph[i]) {
+                adjRev[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+
+        // Push terminal nodes
+        queue<int> q;
+
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        vector<int> safeNodes;
+
+        // Topological Sort
+        while (!q.empty()) {
+
+            int node = q.front();
+            q.pop();
+
+            safeNodes.push_back(node);
+
+            for (auto it : adjRev[node]) {
+
+                indegree[it]--;
+
+                if (indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+
+        // Required sorted order
+        sort(safeNodes.begin(), safeNodes.end());
+
+        return safeNodes;
+    }
+};
